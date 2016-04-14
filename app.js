acfc5e15c95fa44d60de43417303a74449f2b404
Simon Bugert <simon.bugert@googlemail.com>
2016-04-14 12:03:50 +0200

    Add eslint airbnb/base config and make linting pass

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 if (process.env.NODE_ENV !== 'production') { require('dotenv').config(); }
 
-var createServer = require("auto-sni");
-var express      = require("express");
-var app          = express();
-var bodyParser = require('body-parser');
+const createServer = require('auto-sni');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-var settings = {
+const routes = require('./routes');
+
+const settings = {
   email: process.env.EMAIL,
   agreeTos: true,
   debug: true,
   domains: process.env.DOMAIN,
-  forceSSL: process.env.SSL_REDIRECT === 'true' ? true : false,
+  forceSSL: process.env.SSL_REDIRECT === 'true',
   ports: {
     http: process.env.HTTP_PORT,
-    https: process.env.HTTPS_PORT
-  }
+    https: process.env.HTTPS_PORT,
+  },
 };
 
-if(process.env.SSL_REDIRECT !== 'true') {
-  app.use(function (req, res, next) {
+if (process.env.SSL_REDIRECT !== 'true') {
+  app.use((req, res, next) => {
     if (req.protocol === 'http') {
-      res.redirect('https://' + process.env.DOMAIN);
-    }
-    else {
+      res.redirect(`https://${process.env.DOMAIN}`);
+    } else {
       next();
     }
   });
 }
-app.use(bodyParser.json({verify:function(req,res,buf){req.rawBody=buf}}))
 
-require('./routes')(app);
+app.use(bodyParser.json({verify:function(req,res,buf){req.rawBody=buf}})) // eslint-disable-line
+
+routes(app);
 
 createServer(settings, app);
--- routes.js
+++ routes.js
@@ -1,117 +1,115 @@
-var async = require('async');
-var path = require('path');
-var fs = require('fs');
+const async = require('async');
+const path = require('path');
+const fs = require('fs');
 
-var dropbox = require('./lib/dropbox');
-var database = require('./lib/database');
-var image = require('./lib/image');
+const dropbox = require('./lib/dropbox');
+const database = require('./lib/database');
+const image = require('./lib/image');
 
 const imageFormats = ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG'];
 const imageSizes = [
-  { longEdge: '200', suffix: 'small'},
-  { longEdge: '640', suffix: 'medium'},
-  { longEdge: '2000', suffix: 'large'}
+  { width: '320', prefix: 's-' },
+  { width: '640', prefix: 'm-' },
+  { width: '1280', prefix: 'l-' },
 ];
 
-module.exports = function(app){
+const processEntries = (token, entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry['.tag'] === 'file') {
+      console.log(`${entry.path_lower} added or modified.`);
 
-  app.get("/", function(req, res) {
-    res.sendFile('www/signup.html', { root: __dirname });
+      const filename = entry.path_lower.split('/').reverse()[0];
+      const extension = filename.split('.').reverse()[0];
+      console.log(filename, extension);
+
+      if (extension === 'md') {
+        const out = fs.createWriteStream(path.join(process.env.ARTICLE_FOLDER, filename));
+        dropbox.downloadFile(token, entry.path_lower).pipe(out);
+      } else if (imageFormats.indexOf(extension) > -1) { // TODO: check for MIME-type instead
+        const imageStream = dropbox.downloadFile(token, entry.path_lower);
+        imageSizes.forEach((size) => {
+          // var name = filename.replace(/(\.[^\.]*)?$/, '.' + size.suffix + '$1')
+          const name = `${size.prefix}${filename}`;
+          const out = fs.createWriteStream(path.join(process.env.IMAGE_FOLDER, name));
+          image.resize(imageStream, size.width).pipe(out);
+        });
+      } else if (extension === 'gif') { // don't process gifs
+        const out = fs.createWriteStream(path.join(process.env.IMAGE_FOLDER, filename));
+        dropbox.downloadFile(token, entry.path_lower).pipe(out);
+      }
+    } else if (entry['.tag'] === 'deleted') {
+      console.log(`${entry.path_lower} deleted.`);
+    }
   });
+};
 
-  app.get("/redirect", redirect);
-
-  app.get("/webhook", function(req, res) {
-    // echo challenge
-    res.send(req.query.challenge);
-  });
-
-  app.post("/webhook", webhook);
-
-}
-
-function redirect(req, res) {
-  // get token for user and save in redis
-  dropbox.requestToken(req.query.code, function(err, uid, token) {
-    database.setToken(uid, token, function(err) {
-      res.sendFile('www/redirect.html', { root: __dirname });
-    });
-  });
-}
-
-function webhook(req, res) {
-  res.sendStatus(200);
-  var users = req.body.delta.users
-  users.forEach(function(user) {
-    processUser(user);
-  });
-}
-
-function processUser(user) {
-  // console.log('User:', user);
-  database.getTokenAndCursor(user, function(token, cursor) {
-    var has_more = true;
-    var entries = [];
+const processUser = (user) => {
+  console.log('User:', user);
+  database.getTokenAndCursor(user, (token, cursor) => {
+    let hasMore = true;
+    let entries = [];
     async.whilst(
-      function() { return has_more},
-      function(cb) {
+      () => hasMore,
+      (cb) => {
         if (cursor) {
-          dropbox.listFolderContinue(token, cursor, function(err, res){
+          dropbox.listFolderContinue(token, cursor, (err, res) => {
+            if (err) throw err;
             entries = entries.concat(res.body.entries);
-            cursor = res.body.cursor;
-            database.setCursor(user, res.body.cursor)
-            has_more = res.body.has_more;
+            // cursor = res.body.cursor;
+            database.setCursor(user, res.body.cursor);
+            hasMore = res.body.has_more;
             cb(err);
           });
-        }
-        else {
-          dropbox.listFolder(token, function(err, res){
+        } else {
+          dropbox.listFolder(token, (err, res) => {
+            if (err) throw err;
             entries = entries.concat(res.body.entries);
-            cursor = res.body.cursor;
-            database.setCursor(user, res.body.cursor)
-            has_more = res.body.has_more;
+            // cursor = res.body.cursor;
+            database.setCursor(user, res.body.cursor);
+            hasMore = res.body.has_more;
             cb(err);
           });
         }
       },
-      function(err) {
+      (err) => {
+        if (err) throw err;
         processEntries(token, entries);
       }
     );
   });
-}
+};
 
-function processEntries(token, entries) {
-  entries.forEach(function(entry) {
-    console.log(entry);
-    if (entry['.tag'] === 'file') {
-      console.log(entry.path_lower + ' added or modified.');
-
-      var filename = entry.path_lower.split('/').reverse()[0];
-      var extension = filename.split('.').reverse()[0];
-      console.log(filename, extension);
-
-      if (extension === 'md') {
-        var out = fs.createWriteStream(path.join(process.env.ARTICLE_FOLDER, filename));
-        dropbox.downloadFile(token, entry.path_lower).pipe(out);
-      }
-      // check for MIME-type instead
-      else if (imageFormats.indexOf(extension) > -1) {
-        var imageStream = dropbox.downloadFile(token, entry.path_lower);
-        imageSizes.forEach(function(size) {
-          var name = filename.replace(/(\.[^\.]*)?$/, '.' + size.suffix + '$1')
-          var out = fs.createWriteStream(path.join(process.env.IMAGE_FOLDER, name));
-          image.resize(imageStream, size.longEdge).pipe(out);
-        });
-      }
-      // don't process gifs
-      else if (extension === 'gif') {
-        var out = fs.createWriteStream(path.join(process.env.IMAGE_FOLDER, filename));
-        dropbox.downloadFile(token, entry.path_lower).pipe(out);
-      }
-    }
-    else if (entry['.tag'] === 'deleted') {
-      console.log(entry.path_lower + ' deleted.');
-    }
+const redirect = (req, res) => {
+  // get token for user and save in redis
+  dropbox.requestToken(req.query.code, (errGet, uid, token) => {
+    if (errGet) throw errGet;
+    database.setToken(uid, token, (errSet) => {
+      if (errSet) throw errSet;
+      res.sendFile('www/redirect.html', { root: __dirname });
+    });
   });
-}
+};
+
+const webhook = (req, res) => {
+  res.sendStatus(200);
+  const users = req.body.delta.users;
+  users.forEach((user) => {
+    processUser(user);
+  });
+};
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.sendFile('www/signup.html', { root: __dirname });
+  });
+
+  app.get('/redirect', redirect);
+
+  app.get('/webhook', (req, res) => {
+    // echo challenge
+    res.send(req.query.challenge);
+  });
+
+  app.post('/webhook', webhook);
+};
